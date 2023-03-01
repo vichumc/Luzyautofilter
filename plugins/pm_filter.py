3ebe10dcb6dc592c00d1ea6773be770c17d15e72
vichumc <100103080+vichumc@users.noreply.github.com>
2023-03-01 16:20:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,7 +708,7 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"ᴩᴀɢᴇ", callback_data="pages"),
-             InlineKeyboardButton(text=f"1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
+             InlineKeyboardButton(text=f"1/{math.ceil(int(total_results) /6)}", callback_data="pages"),
              InlineKeyboardButton(text="NEXT", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
