e296b2326a12885125b3188ad888d3eee8cde342
vichumc <100103080+vichumc@users.noreply.github.com>
2023-03-01 11:40:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -846,15 +846,15 @@ async def manual_filters(client, message, text=False):
                     if fileid == "None":
                         if btn == "[]":
                             await client.send_message(group_id, reply_text, disable_web_page_preview=True)
-                            else:
+                        else:
                             button = eval(btn)
-                            ml = await client.send_message(
+                            await client.send_message(
                                 group_id,
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button),
                                 reply_to_message_id=reply_id
-                           ) 
+                            )
                     elif btn == "[]":
                         await client.send_cached_media(
                             group_id,
@@ -870,7 +870,7 @@ async def manual_filters(client, message, text=False):
                             reply_markup=InlineKeyboardMarkup(button),
                             reply_to_message_id=reply_id
                         )
-                        except Exception as e:
+                except Exception as e:
                     logger.exception(e)
                 break
     else:
