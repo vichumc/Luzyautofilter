28b630cd6ef9bada121cd5950b4c64aa5ccb8212
Vishnu V <100103080+vichumc@users.noreply.github.com>
2024-10-23 18:16:36 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,7 +37,7 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('🤍 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐆𝐫𝐨𝐮𝐩 🤍', url='https://t.me/+TGPAvILX9_thNGU9'),
+            InlineKeyboardButton('🤍 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐆𝐫𝐨𝐮𝐩 🤍', url='https://t.me/+M60R1C_kYx84NDU9'),
             InlineKeyboardButton('📂 Main Channel 📂', url='https://t.me/+G2ZP7VAY6QMzYmQ1')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
